77062731ed87c9167f0f624d80329386c60c69b7
Daniel Ge <dange@seas.upenn.edu>
2013-03-10 17:50:20 -0400

    Fix Google form size on mobile version

--- _register.php
+++ _register.php
@@ -3,7 +3,7 @@
 
 <h1>Registration</h1>
 
-<iframe src="https://docs.google.com/forms/d/1KfuaRNij513UwAFG5mgZ5O-2eXJUiKbi0ODJ4LDYcqc/viewform?embedded=true" width="970" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+<iframe src="https://docs.google.com/forms/d/1KfuaRNij513UwAFG5mgZ5O-2eXJUiKbi0ODJ4LDYcqc/viewform?embedded=true" height="500" frameborder="0" marginheight="0" marginwidth="0" style="max-width:970;">Loading...</iframe>
 
 </div>
 </div>
