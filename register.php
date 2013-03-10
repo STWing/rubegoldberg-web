f2a1a6ed37bdb047ac7eb01fbe3f2f62f24a30a4
Daniel Ge <dange@seas.upenn.edu>
2013-03-10 15:26:15 -0400

    Selectively show Sign up button

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
 	$main = "_register.php";
+  $dont_include = true;
 	include("template.php");
 ?>
--- template.php
+++ template.php
@@ -41,7 +41,11 @@
           <div id="banner-title-wrapper">
             <h1 id="site-title">Rube Goldberg</h1>
             <p id="site-tagline">[04.06.2013 - 04.07.2013]</p>
+            <? if ($dont_include) { ?>
+            <a href="register.php" class="large button red hidden-space">Sign up now</a>
+            <? } else { ?>
             <a href="register.php" class="large button red">Sign up now</a>
+            <? } ?>
           </div>
         </div>
       </div>
