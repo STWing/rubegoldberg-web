5b5525acc98fa5fbbe628101c39d31cdacbfb623
Daniel Ge <dange@seas.upenn.edu>
2013-03-10 13:58:19 -0400

    Add registration page

---
+++ _register.php
@@ -0,0 +1,9 @@
+<div class="row">
+<div class="large-12 columns">
+
+<h1>Registration</h1>
+
+<iframe src="https://docs.google.com/forms/d/1KfuaRNij513UwAFG5mgZ5O-2eXJUiKbi0ODJ4LDYcqc/viewform?embedded=true" width="970" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+</div>
+</div>
---
+++ register.php
@@ -0,0 +1,4 @@
+<?php
+	$main = "_register.php";
+	include("template.php");
+?>
