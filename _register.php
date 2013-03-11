b0340aafea2f492197c228fdb1acaa95fdcdaa41
Daniel Ge <dange@seas.upenn.edu>
2013-03-10 21:04:54 -0400

    Fix form sizing

--- _register.php
+++ _register.php
@@ -1,9 +1,7 @@
 <div class="row">
 <div class="large-12 columns">
-
 <h1>Registration</h1>
 
-<iframe src="https://docs.google.com/forms/d/1KfuaRNij513UwAFG5mgZ5O-2eXJUiKbi0ODJ4LDYcqc/viewform?embedded=true" height="500" frameborder="0" marginheight="0" marginwidth="0" style="max-width:970;">Loading...</iframe>
-
+<iframe src="https://docs.google.com/forms/d/1KfuaRNij513UwAFG5mgZ5O-2eXJUiKbi0ODJ4LDYcqc/viewform?embedded=true" height="500" frameborder="0" marginheight="0" marginwidth="0" class="large-12">Loading...</iframe>
 </div>
 </div>
