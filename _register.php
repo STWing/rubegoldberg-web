e338fcddb0e689944ae8f73e1cf6c3ba1efd0e4c
Daniel Ge <dange@seas.upenn.edu>
2013-03-11 23:22:16 -0400

    Add t-shirt logo pictures

--- _register.php
+++ _register.php
@@ -3,5 +3,20 @@
 <h1>Registration</h1>
 
 <iframe src="https://docs.google.com/forms/d/1KfuaRNij513UwAFG5mgZ5O-2eXJUiKbi0ODJ4LDYcqc/viewform?embedded=true" height="500" frameborder="0" marginheight="0" marginwidth="0" class="large-12">Loading...</iframe>
+<hr />
+</div>
+
+<div id="t-shirt-logos" class="large-12 columns">
+<h1>T-shirt logos</h1>
+<p>To help us personalize your T-shirts, please send us your T-shirt logo by
+  Sunday, March 31. Here are some examples of T-shirt logos from past years.</p>
+
+<ul class="large-block-grid-4">
+  <li><a href="img/logos/t-shirt-1.jpg"><img src="img/t-shirt-1.jpg"></a></li>
+  <li><a href="img/logos/t-shirt-2.jpg"><img src="img/t-shirt-2.jpg"></a></li>
+  <li><a href="img/logos/t-shirt-3.jpg"><img src="img/t-shirt-3.jpg"></a></li>
+  <li><a href="img/logos/t-shirt-4.jpg"><img src="img/t-shirt-4.jpg"></a></li>
+</ul>
+
 </div>
 </div>
